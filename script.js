78f000c25e6f9215d5373d5fcf10b5e717cfaa9a
quzefeng2 <1814022210@qq.com>
2025-06-14 11:15:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ noButton.addEventListener("click", function() {
     }
 
     // 图片变化（前 5 次变化）
-    if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
+    if (clickCount === 1) mainImage.src = "images/DSC02491.JPG"; // 震惊
     if (clickCount === 2) mainImage.src = "images/think.png";   // 思考
     if (clickCount === 3) mainImage.src = "images/angry.png";   // 生气
     if (clickCount === 4) mainImage.src = "images/crying.png";  // 哭
