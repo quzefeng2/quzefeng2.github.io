1503f8437d48b9b5e9c7e04f7c885183b3cad405
quzefeng2 <1814022210@qq.com>
2025-06-14 10:51:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const noTexts = [
     "要不再想想？", 
     "不许选这个！ ", 
     "我会很伤心…", 
-    "不行:("
+    "再考虑考虑呐QMQ…", 
+    "不行:(",
 ];
 
 // No 按钮点击事件
@@ -32,7 +33,7 @@ noButton.addEventListener("click", function() {
     questionText.style.transform = `translateY(-${moveUp}px)`;
 
     // No 文案变化（前 5 次变化）
-    if (clickCount <= 5) {
+    if (clickCount <= 6) {
         noButton.innerText = noTexts[clickCount - 1];
     }
 
